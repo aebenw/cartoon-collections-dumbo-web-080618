3585dd3de055677121e014f8ded4bac7fe4039fa
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 17:19:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(arr)
   arr.each_with_index {|dwarf, i| puts "#{i + 1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
 end
 
