8dbf8fced41c70292aaac7e54a0420aadeff6b0e
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 17:28:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(arr)
   arr.any? {|plan| plan.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.each {|food| 
 end
